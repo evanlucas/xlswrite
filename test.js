444d999dec58ea1d20025883a0ba43539b6e7c63
Evan Lucas <evanlucas@me.com>
2013-11-21 23:57:22 -0600

    Initial commit

---
+++ test.js
@@ -0,0 +1,13 @@
+var XLS = require('./lib')
+  , xls = new XLS(true)
+
+console.log(xls.contentType(3))
+
+console.log(xls.rels())
+
+var sheets = ['This is sheet 1', 'Sheet 2', 'Test']
+console.log(xls.app(sheets, 'curapps'))
+
+var name = 'Evan'
+
+console.log(xls.core(name))
